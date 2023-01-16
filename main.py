bce6c3b67d3bf66ea8ca6680116482dd2fb80277
Coin Data School <coindataschool@gmail.com>
2023-01-16 20:42:28 +0900

    ...

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ if last_avail_date < end_date:
 # st.dataframe(df.tail())
 
 # subset prices with user specificed start and end dates
-df = df.loc[start_date:end_date]
+df = df.loc[start_date:end_date, :]
 
 # calculate daily returns
 simple_rets = df.pct_change().dropna()
